08de7752a308d08456ab13e66824ecff8777d89e
mykytko <nd235471@gmail.com>
2022-09-30 16:48:13 +0300

    Implement class Car and loading data from csv

--- src/main/java/io/github/mykytko/Car.java
+++ src/main/java/io/github/mykytko/Car.java
@@ -1,2 +1,74 @@
-package io.github.mykytko;public class Car {
+package io.github.mykytko;
+
+public final class Car {
+
+    private static int counter = 0;
+
+    private final Integer id;
+    private final String brand;
+    private final String model;
+    private final Integer year;
+    private final Integer color;
+    private final Integer price;
+    private final String registrationNumber;
+
+    public Car(String brand, String model, int year,
+               int color, int price, String registrationNumber) {
+
+        id = counter;
+        counter++;
+
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.registrationNumber = registrationNumber;
+    }
+
+    @Override
+    public String toString() {
+        var builder = new StringBuilder();
+        var lineSeparator = System.getProperty("line.separator");
+
+        builder.append("Brand: ").append(brand);
+        builder.append(lineSeparator);
+        builder.append("Model: ").append(model);
+        builder.append(lineSeparator);
+        builder.append("Year: ").append(year);
+        builder.append(lineSeparator);
+        builder.append("Color: #").append(Integer.toHexString(color));
+        builder.append(lineSeparator);
+        builder.append("Registration number: ").append(registrationNumber);
+
+        return builder.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
 }
--- src/main/java/io/github/mykytko/Main.java
+++ src/main/java/io/github/mykytko/Main.java
@@ -1,7 +1,77 @@
-package org.example;
+package io.github.mykytko;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var url = "https://raw.githubusercontent.com/rashida048/Datasets/master/cars.csv";
+        var fileName = "cars.csv";
+
+        List<String> lines;
+        try {
+            downloadData(url, fileName);
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        var cars = new ArrayList<Car>();
+        var rand = new Random();
+        for (int i = 1; i < lines.size(); i++) {
+            var splits = lines.get(i).split(",");
+            var brand = splits[1];
+            var model = splits[2];
+            var year = Integer.parseInt(splits[0]);
+            var color = rand.nextInt(0xffffff + 1);
+            var price = 10000 + rand.nextInt(400) * 100 - rand.nextInt(1);
+            var registrationNumber = generateRegistrationNumber();
+
+            var car = new Car(brand, model, year, color, price, registrationNumber);
+            cars.add(car);
+        }
+
+        for (var car : cars) {
+            System.out.println(car);
+            System.out.println();
+        }
     }
+
+    private static String generateRegistrationNumber() {
+        return "";
+    }
+
+    private static ArrayList<Car> taskA(ArrayList<Car> cars) {
+        return null;
+    }
+
+    private static ArrayList<Car> taskB(ArrayList<Car> cars) {
+        return null;
+    }
+
+    private static ArrayList<Car> taskC(ArrayList<Car> cars) {
+        return null;
+    }
+
+    private static void downloadData(String url, String fileName) throws IOException {
+        var readableByteChannel = Channels.newChannel(new URL(url).openStream());
+        var fileOutputStream = new FileOutputStream(fileName);
+        var fileChannel = fileOutputStream.getChannel();
+
+        fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        fileChannel.close();
+        fileOutputStream.close();
+        readableByteChannel.close();
+    }
+
 }
