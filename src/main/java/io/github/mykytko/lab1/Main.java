5780ebb7be31938e41e664512704dff7db5a506c
Mykyta Krainik <nikinta2003@gmail.com>
2022-10-01 16:19:03 +0300

    Tasks a and b were implemented

--- src/main/java/io/github/mykytko/lab1/Main.java
+++ src/main/java/io/github/mykytko/lab1/Main.java
@@ -10,8 +10,11 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.time.YearMonth;
 
 public class Main {
+    private static ArrayList<Car> cars = new ArrayList<Car>();
+
     public static void main(String[] args) {
 
         var url = "https://raw.githubusercontent.com/rashida048/Datasets/master/cars.csv";
@@ -26,8 +29,8 @@ public class Main {
             return;
         }
 
-        var cars = new ArrayList<Car>();
         var rand = new Random();
+
         for (int i = 1; i < lines.size(); i++) {
             var splits = lines.get(i).split(",");
             var brand = splits[1];
@@ -45,18 +48,55 @@ public class Main {
             System.out.println(car);
             System.out.println();
         }
+
+        ArrayList<Car> carsWithBrand = taskA("tesla");
+        ArrayList<Car> carsWithModelYear = taskB("MODEL S (60 kWh battery)", 8);
+
+        System.out.println("Cars with a specific brand:\n");
+
+        for (var car : carsWithBrand) {
+          System.out.println(car);
+          System.out.println();
+        }
+
+        System.out.println("\nCars with a specific model that were used for n or more years:\n");
+
+        for (var car : carsWithModelYear) {
+          System.out.println(car);
+          System.out.println();
+        }
     }
 
     private static String generateRegistrationNumber() {
         return "";
     }
 
-    private static ArrayList<Car> taskA(ArrayList<Car> cars) {
-        return null;
+    private static ArrayList<Car> taskA(String brand) {
+        ArrayList<Car> carsList = new ArrayList<Car>();
+
+        for (Car car : cars) {
+          if (car.getBrand().equalsIgnoreCase(brand)) {
+            carsList.add(car);
+          }
+        }
+
+        return carsList;
     }
 
-    private static ArrayList<Car> taskB(ArrayList<Car> cars) {
-        return null;
+    private static ArrayList<Car> taskB(String model, int usedFor) {
+      ArrayList<Car> carsList = new ArrayList<Car>();
+      int currentYear = YearMonth.now().getYear();
+
+      for (Car car : cars) {
+        if (
+          car.getModel().equalsIgnoreCase(model)
+          && (currentYear - car.getYear()) > usedFor
+        ) {
+          carsList.add(car);
+        }
+      }
+
+      return carsList;
     }
 
     private static ArrayList<Car> taskC(ArrayList<Car> cars) {
@@ -73,5 +113,4 @@ public class Main {
         fileOutputStream.close();
         readableByteChannel.close();
     }
-
 }
